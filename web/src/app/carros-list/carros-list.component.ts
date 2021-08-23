93482cfa2858489c560c7b713583ed8a4e6aa945
Joao Victor <costa.jvsc@gmail.com>
2021-08-23 11:54:15 -0300

    feat(web): Adicionando listagem de carros

--- web/src/app/carros-list/carros-list.component.ts
+++ web/src/app/carros-list/carros-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CarroService } from '../service/carro.service';
+import { CategoriaService } from '../service/categoria.service';
+import { MarcaService } from '../service/marca.service';
 
 @Component({
   selector: 'app-carros-list',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrosListComponent implements OnInit {
 
-  constructor() { }
+
+  Carros: any = [];
+  Categorias: any = [];
+  Marcas: any = [];
+  @Input() novoCarro = { modelo: '', potencia: 0, autonomia: 0, peso: 0, ano: 0, categoriaId: 0,  marcaId: 0}
+
+  constructor(
+    public carroService: CarroService,
+    public categoriaService: CategoriaService,
+    public marcaService: MarcaService
+  ) { }
 
   ngOnInit(): void {
+    this.LoadCarros();
+    this.LoadCategorias();
+    this.LoadMarcas();
+  }
+
+  LoadCarros(){
+    return this.carroService.getCarros().subscribe((data: {}) => {
+      this.Carros = data;
+    })
+  }
+
+  LoadCategorias(){
+    return this.categoriaService.getCategorias().subscribe((data: {}) => {
+      this.Categorias = data;
+    })
+  }
+
+  LoadMarcas(){
+    return this.marcaService.getMarcas().subscribe((data: {}) => {
+      this.Marcas = data;
+    })
+  }
+
+  adicionarCarro(){
+      this.carroService.createCarro(this.novoCarro).subscribe((data: {}) => {
+        alert('Carro adicionado com sucesso');
+      })
+  }
+
+  removerCarro(id : any){
+    if(window.confirm('Tem certeza que deseja realizar esta operação?')){
+      this.carroService.deleteCarro(id).subscribe((data: any) => {
+        alert('Carro excluido com sucesso')
+        this.LoadCarros()
+      })
+    }
   }
 
 }
